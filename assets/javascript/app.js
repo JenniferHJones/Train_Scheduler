25e2f954b28faead6defcffd21130e0829ee4c37
JJ <jenjones@ucdavis.edu>
2019-02-02 22:27:05 -0800

    updated index.html, read me, css files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // initialize Firebase
     var config = {
         apiKey: "AIzaSyAS2hCzuDydxISQ5g7eM7zBOtgWbkvcVjA",
         authDomain: "jj-s-project.firebaseapp.com",
@@ -13,47 +14,67 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
+    // on click function for adding a new train
     $("#add-train").on("click", function () {
         event.preventDefault();
 
-        var empName = $("#employee-name-input").val().trim();
-        var empRole = $("#role-input").val().trim();
-        var empStartDate = $("#start-input").val().trim();
-        var empRate = $("#rate-input").val().trim();
+        var trainName = $("#trainName").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrainTime = $("#firstTime").val().trim();
+        var frequency = $("#frequency").val().trim();
         console.log("clicked");
 
+        // object to save user's entries to Firebase db
         database.ref().push({
-            name: empName,
-            role: empRole,
-            startDate: empStartDate,
-            rate: empRate,
+            name: trainName,
+            destination: destination,
+            time: firstTrainTime,
+            frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         })
     });
 
+    var currentTime = moment().format("LT");
+    $("#currentTime").append(currentTime);
+    console.log("current time",currentTime);
+
+    // current time
+    var now = moment();
+
+    // calling values from Firebase
     database.ref().on("child_added", function (db) {
         console.log("child_added");
         var dbObj = db.val();
-        // must start with this line to take your date, which is a string, and convert it to time
-        var startMoment = moment(dbObj.startDate, "MM/DD/YYYY");
-        // this line will calculate the difference between now and the date
-        var monthCount = moment().diff(startMoment, "months");
+        console.log("user's input", dbObj.time);
 
-        var totalBilled = dbObj.rate*monthCount;
+        // create moment.js object for train time
+        var startTime = moment(dbObj.time, "HH:mm");
+        console.log("start time: ", startTime.format("HH:mm"));
+        
+        // change frequency string into a number
+        var trainFrequency = parseInt(dbObj.frequency);
+        console.log("frequency", trainFrequency);
 
+        // calculate diffference between now and the start time minus frequency to get remainder
+        var minutesAway = trainFrequency - now.diff(startTime, "minutes") % trainFrequency;
+        console.log("minutes away", minutesAway);
+
+        // calculate next train's arrival by adding above calculation to now
+        var nextArrival = moment().add(minutesAway, "minutes");
+        console.log("arrival", nextArrival.format("hh:mm A"));
+
+        // create new row to store the data & append it
         var newRow = $("<tr>");
         newRow.append($("<td>" + dbObj.name + "</td>"));
-        newRow.append($("<td>" + dbObj.role + "</td>"));
-        newRow.append($("<td>" + dbObj.startDate + "</td>"));
-        newRow.append($("<td>" + monthCount + "</td>"));
-        newRow.append($("<td>" + dbObj.rate + "</td>"));
-        newRow.append($("<td>" + totalBilled + "</td>"));
+        newRow.append($("<td>" + dbObj.destination + "</td>"));
+        newRow.append($("<td>" + dbObj.frequency + "</td>"));
+        newRow.append($("<td>" + nextArrival.format("hh:mm A") + "</td>"));
+        newRow.append($("<td>" + minutesAway + "</td>"));
 
+        // append new row
         $("tbody").append(newRow);
 
     }, function (e) {
         alert(e)
     });
-
-
 })
